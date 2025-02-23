669d8608b1ed2d66acac2b9853855efa111ea7da
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-02-23 21:59:08 +0000

    fix: changing project frameworks

--- new-app/app/Types/IWeather.tsx
+++
@@ -1,7 +0,0 @@
-export default interface IWeather {
-    temp: number,
-    temp_min: number,
-    temp_max: number,
-    humidity: number,
-    error: "Não foi possível localizar a cidade" | "Cidade não encontrada"
-};
