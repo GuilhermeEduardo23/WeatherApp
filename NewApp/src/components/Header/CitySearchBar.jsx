64db6c43179482ccf56001c409749b289a4003f6
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-07-06 18:36:40 +0100

    feat: created a new project

---
+++ NewApp/src/components/Header/CitySearchBar.jsx
@@ -0,0 +1,20 @@
+// Style
+import "../Header/CitySearchBar.scss";
+
+// Icon
+import { IoIosSearch } from "react-icons/io";
+
+const CitySearchBar = () => {
+  return (
+    <div>
+      <h1 className="title">Previsão do Tempo</h1>
+
+      <div className="searchBar">
+        <input type="text" placeholder="Buscar cidade..." />
+        <IoIosSearch />
+      </div>
+    </div>
+  );
+};
+
+export default CitySearchBar;
